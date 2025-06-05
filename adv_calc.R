9591c112905996e84f6db5320a61f71c07d87810
breesie <breesie2@gmail.com>
2025-06-05 13:25:33 -0700

    test commit

---
+++ adv_calc.R
@@ -0,0 +1,13 @@
+#calculate the growth advantage from the coefficients of two models
+library(mvtnorm)
+library(nnet)
+
+
+logspace <- function(clademod, pangomod)
+
+function(clademod, pangomod, coeff){
+  
+  
+  
+  
+}
