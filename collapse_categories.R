1400daf9e66f0b055a5441d50e68bc27157101ff
breesie <breesie2@gmail.com>
2025-06-17 13:34:41 -0700

    emtrends start

---
+++ collapse_categories.R
@@ -0,0 +1,4 @@
+#function to collapse the number of categories to model and transform data into inputs for nnet model
+#takes an x value (time) and y value (sequence count)
+
+multinom.collapse <- function(ncat,x,y){}
